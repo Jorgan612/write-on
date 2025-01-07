f2ca24b65d7f6d89f9e45885cda515971e4834e2
Jessica Organ <jorgan612@gmail.com>
2025-01-07 06:18:52 -0700

    added dropdown setup
    Added dropdown styling and basic functionality to hide/show on menu icon click.

--- src/header/header.js
+++ src/header/header.js
@@ -1,7 +1,17 @@
+import Dropdown from '../dropdown/Dropdown';
 import './header.scss';
 import menuIcon from "../assets/icons/menu_icon.svg";
+import { useState } from 'react';
 
 function Header() {
+
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const handleMenuClick = () => {
+        setMenuClicked( prev => !prev ) 
+    }
+
+
     return (
         <div className="header-container">
             <div className='app-title'>Write On!</div>
@@ -11,8 +21,11 @@ function Header() {
                 <div className='menu-item'>Goals</div>
                 <div className='menu-item'>Profile</div>
             </div>
-            <div className='mobile-menu-icon'>
-                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#D9D9D9"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+            <div className='dropdown-container'>
+                <div className='menu-icon'>
+                    <svg onClick={handleMenuClick} xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#D9D9D9"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+                </div>
+                { menuClicked ? <Dropdown menuClicked={menuClicked} /> : <div className='hidden'></div> }
             </div>
         </div>
     );
