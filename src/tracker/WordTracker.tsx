868f1539dce365cc29623be66bff06e4f3b779ee
Jessica Organ <Jorgan612@gmail.com>
2026-01-08 14:33:47 -0700

    Implement functionality for adding word count and updating total word count. Currently data does not persist.

--- src/tracker/WordTracker.tsx
+++ src/tracker/WordTracker.tsx
@@ -1,19 +1,28 @@
-import { useState } from 'react';
+import { useState, FormEvent, ChangeEvent } from 'react';
 import './WordTracker.scss';
 
 function WordTracker() {
-  const [newWords, setNewWords] = useState(0);
+  const [newWords, setNewWords] = useState<string>('');
+  let [total, setTotal] = useState<number>(0);
 
-  const addWords = () => {
-    console.log('setNewWords', newWords);
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewWords(e.target.value);
+  }
+
+  const updateWordCount = (e: FormEvent) => {
+    e.preventDefault();
+    setTotal(prev => prev + Number(newWords));
+    setNewWords('');
   }
 
   return (
-    <section className='word-tracker-container'> 
+    <section className='word-tracker-container'>
       <label>Update Word Count</label>
-      <input  />
-      <button>Add</button>
-      <p>Congrats! You've added {newWords} words to your WIP!</p>
+      <form onSubmit={updateWordCount}>
+        <input placeholder='####' type='number' value={newWords} onChange={handleInputChange}/>
+        <button type='submit'>Add</button>
+      </form>
+      <p>Today's total: {total}</p>
     </section>
   )
 
