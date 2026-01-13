18982ed5b419d058d04efdbc05b64f6cfefa123d
Jessica Organ <jorgan612@gmail.com>
2026-01-13 15:37:04 -0700

    installed date fns and calculated months in the year to begin calendar grid.

--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
 import './Calendar.scss';
+import {getDay, getMonth, getDaysInMonth, eachMonthOfInterval, startOfMonth, startOfYear, endOfYear, endOfMonth, startOfWeek, endOfWeek, eachDayOfInterval, format} from 'date-fns';
 
 
 function Calendar() {
+    const today = new Date();
+
+    const monthsInYear = eachMonthOfInterval({
+        start: startOfYear(today),
+        end: endOfYear(today)
+    }).map(monthDate => {
+     return {
+        monthName: format(monthDate, 'MMMM'),
+        daysInMonth: getDaysInMonth(monthDate),
+        startDayOfWeek: getDay(startOfMonth(monthDate)), 
+        year: format(monthDate, 'yyyy')
+        };
+    });
+
+    // console.log('TEST', monthsInYear);
+
+    // useEffect(() => {
+        
+    // }, [])
 
     return (
         <div className="calendar-container">
