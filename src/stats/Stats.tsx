9e0a9f166419955a819cc3cd921484f9773819d2
Jessica Organ <jorgan612@gmail.com>
2026-02-09 12:26:07 -0700

    Modified logic from 7 days to 30 day timeframe for updateMonthWordCount logic.

--- src/stats/Stats.tsx
+++ src/stats/Stats.tsx
@@ -1,5 +1,5 @@
 import './Stats.scss';
-import { format, subDays } from 'date-fns';
+import { format, subDays, getMonth } from 'date-fns';
 import "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
@@ -39,14 +39,55 @@ const updateSevenDayWordCount = () => {
 
 const updateMonthWordCount = () => {
     // logic for month graph
+    const lastThirtynDays = Array.from({ length: 30 }).map((_, i) => {
+    return subDays(new Date(), 30 - i);
+  });
+
+  const displayLabels = lastThirtynDays.map(date => format(date, 'MMM d'));
+
+  const totals = lastThirtynDays.map(date => {
+    const dateString = format(date, 'yyyy-MM-dd');
+    return combinedEntries[dateString] || 0;
+  });
+
+  return {
+    labels: displayLabels,
+    datasets: [
+      {
+        label: 'Words',
+        data: totals,
+        borderColor: '#527199',
+        backgroundColor: '#263b56',
+        tension: 0.3,
+        fill: true,
+      },
+    ],
+  };
 }
+
 
 return (
     <div className="stats-container">
         <div className='graph-container'>
-            <p>Previous seven days overview.</p>
+            <p>Last 7 days overview.</p>
             {Object.keys(combinedEntries).length > 0 ? (
             <Line data={updateSevenDayWordCount()} />
+          ) : (
+            <p>No data recorded yet. Start writing!</p>
+          )}
+        </div>
+        <div className='graph-container'>
+            <p>Last 30 days overview.</p>
+            {Object.keys(combinedEntries).length > 0 ? (
+            <Line data={updateMonthWordCount()} />
+          ) : (
+            <p>No data recorded yet. Start writing!</p>
+          )}
+        </div>
+        <div className='graph-container full-year'>
+            <p>Year overview.</p>
+            {Object.keys(combinedEntries).length > 0 ? (
+            <Line data={updateMonthWordCount()} />
           ) : (
             <p>No data recorded yet. Start writing!</p>
           )}
