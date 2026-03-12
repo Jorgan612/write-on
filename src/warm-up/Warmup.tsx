0905490ff7e020ba6f10bef7bc4740bd36afa098
Jessica Organ <jorgan612@gmail.com>
2026-03-12 17:17:08 -0600

    Refactored code to use localStorage to persist prompts on reload. Added list to confirm prompts persist and to start working on separate issue.

--- src/warm-up/Warmup.tsx
+++ src/warm-up/Warmup.tsx
@@ -1,15 +1,28 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 import './warmup.scss';
-import { prompts, excerpts } from '../datasets/prompts';
+import { prompts as InitialPrompts, excerpts } from '../datasets/prompts';
 
 function Warmup() {
     const [userInput, setUserInput] = useState<string>("");
+    const [promptList, setPromptList] = useState(() => {
+        const saved = localStorage.getItem("user_prompts");
+        return saved ? JSON.parse(saved) : InitialPrompts
+    });
+
+    useEffect(() => {
+        localStorage.setItem("user_prompts", JSON.stringify(promptList));
+    }, [promptList]);
     
     const handleNewPrompt = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setUserInput(e.target.value);
-    }
+    };
     
-    const addNewPrompt = (() => {
+    const addNewPrompt = () => {
+
+        if (!userInput.trim()) {
+            return;
+        }
+
         let newPrompt = {
             id: Date.now(),
             prompt: userInput,
@@ -17,9 +30,10 @@ function Warmup() {
             discarded: 0
         }
 
-        prompts.push(newPrompt);
+        setPromptList([...promptList, newPrompt]);
+
         setUserInput("");
-    })
+    }
 
     return (
         <div className="warm-up-container">
@@ -28,6 +42,11 @@ function Warmup() {
             </label>
             <textarea id="prompt" name="prompt" value={userInput} onChange={handleNewPrompt}></textarea>
             <button onClick={addNewPrompt}>Add</button>
+            <ul>
+                {promptList.map((p: any) => (
+                    <li key={p.id}>{p.prompt}</li>
+                ))}
+            </ul>
         </div>
     );
 }
