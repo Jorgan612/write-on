6e24058330b3540e58ef2a6af7056a3b3ee45dd5
Jessica Organ <jorgan612@gmail.com>
2026-04-13 17:03:41 -0600

    Begin work on weekly word count current word count logic.

--- src/goals/ActiveGoals.tsx
+++ src/goals/ActiveGoals.tsx
@@ -1,6 +1,7 @@
 import '../goals/activeGoals.scss';
 import { ChangeEvent } from 'react';
-import { isSunday, nextSaturday } from 'date-fns';
+import { isSunday, nextSaturday, isSameWeek, isThisWeek, startOfWeek, endOfWeek, getWeek } from 'date-fns';
+import dayjs from 'dayjs';
 import { FaEdit } from "react-icons/fa";
 import { User, UserProps } from '../interfaces/interfaces';
 import { useState, useEffect } from 'react';
@@ -9,12 +10,21 @@ import MenuDropdown from '../dropdown/MenuDropdown';
 import Form from '../forms/Form';
 
 function ActiveGoals({currentUser, setCurrentUser, combinedEntries}: UserProps) {
+    const currentDay = dayjs().format('YYYY-MM-DD').split('-');
+    const currentWeek = getWeek(new Date(Number(currentDay[0]), Number(currentDay[1]), Number(currentDay[2])))
+    const isCurrentWeek = isThisWeek(new Date(Number(currentDay[0]), (Number(currentDay[1]) - 1), Number(currentDay[2])))
 
     const [weeklyTotal, setWeeklyTotal] =  useState<number | ''>(currentUser.goals[0]?.total || '');
     const [frequencyTotal, setFrequencyTotal] =  useState<number | ''>(currentUser.goals[1]?.total || '');
     const [overallTotal, setOverallTotal] =  useState<number | ''>(currentUser.goals[2]?.total || '');
 
     const [weekRange, setWeekRange] = useState<[]>([]);
+    // console.log('at 0', currentDay[0])
+    // console.log('at 1', (Number(currentDay[1]) - 1))
+    // console.log('at 2', currentDay[2])
+
+    console.log('currentWeek', currentWeek)
+    console.log('isCurrentWeek', isCurrentWeek)
 
     const defaultGoals = [{
         name: 'Weekly Word Count',
@@ -40,6 +50,7 @@ function ActiveGoals({currentUser, setCurrentUser, combinedEntries}: UserProps) 
 
     
     useEffect(() => {
+        console.log('currentDay', currentDay)
 
         if (!currentUser.goals || currentUser.goals.length === 0) {
             setCurrentUser(prev => ({
@@ -79,6 +90,11 @@ function ActiveGoals({currentUser, setCurrentUser, combinedEntries}: UserProps) 
             setOverallTotal(newTotal);
         }
     };
+
+    // weekly word count:
+    //what about just using getWeek and checking current date against isSameweek? while iterating over combinedEntries?
+    // save dates in combinedEntries (based on isThisWeek boolean to a new array to get total word count for week?
+
 
     return (
         <div className="goals-container">
