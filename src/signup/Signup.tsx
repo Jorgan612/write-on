9203f470268d21f3b0753ce42f6869d6fb76521e
Jessica Organ <jorgan612@gmail.com>
2026-04-16 16:55:04 -0600

    Added optional details section to obtain profile details if added. These will not be required to create an account, but to help fill out details for user object.

--- src/signup/Signup.tsx
+++ src/signup/Signup.tsx
@@ -34,39 +34,50 @@ function Signup({ setSignedIn }: { setSignedIn: (val: boolean) => void }) {
                 <h1 onClick={returnToLandingPage}>Write On <FaPenFancy /></h1>
             </div>
             <div className='email'>
-                <label htmlFor='email'>Email:</label>
+                <label htmlFor='email'><span className='asterisk'>*</span>Email:</label>
                 <div>
-                    <input type="text" id='email' placeholder="Username" />
+                    <input required type="text" id='email' placeholder="Username" />
                 </div>
-                <label htmlFor='confirm-email'>Confirm Email:</label>
+                <label htmlFor='confirm-email'><span className='asterisk'>*</span>Confirm Email:</label>
                 <div>
-                    <input type="text" id='confirm-email' placeholder="Username" />
+                    <input required type="text" id='confirm-email' placeholder="Username" />
                 </div>
 
             </div>
             <div className='username'>
-                <label htmlFor='username'>Username:</label>
+                <label htmlFor='username'><span className='asterisk'>*</span>Username:</label>
                 <div>
-                    <input type="text" id='username' placeholder="Username" />
+                    <input required type="text" id='username' placeholder="Username" />
                 </div>
             </div>
             <div className='password'>
-                <label htmlFor='password'>Password:</label>
+                <label htmlFor='password'><span className='asterisk'>*</span>Password:</label>
                 <div>
-                    <input type="text" id='password' placeholder="Password" />
+                    <input required type="text" id='password' placeholder="Password" />
                     <span className='visibility-icon' onClick={toggleVisibility}>
                         {isVisible ? <FaRegEye className='icon'/> : <FaRegEyeSlash className='icon' />}
                     </span>
                 </div>
-                <label htmlFor='confirm-password'>Confirm Password:</label>
+                <label htmlFor='confirm-password'><span className='asterisk'>*</span>Confirm Password:</label>
                 <div>
-                    <input type="text" id='confirm-passowrd' placeholder="Password" />
+                    <input required type="text" id='confirm-passowrd' placeholder="Password" />
                     <span className='visibility-icon' onClick={toggleVisibility}>
                         {isVisible ? <FaRegEye className='icon'/> : <FaRegEyeSlash className='icon' />}
                     </span>
                 </div>
             </div>
-            <span className='goal-msg1'>Let's set a few goals...</span>
+            <span className='msg1'>Optional Details</span>
+            <div className='website'>
+                <label htmlFor='website-name'>Website</label>
+                <div>
+                    <input type="text" id='website-name' placeholder="Site name" />
+                </div>
+                <label htmlFor='website-url'>URL</label>
+                <div>
+                    <input type="text" id='website-url' placeholder="URL address" />
+                </div>
+            </div>
+            <span className='msg1'>Let's set a few goals...</span>
             <div className='goals'>
                 <label htmlFor='weekly-wc'>Weekly Word Count:</label>
                 <div>
