64e7d6053b90b07863fc6e11c4f68a72a5bdc09c
Jessica Organ <jorgan612@gmail.com>
2026-04-17 11:21:11 -0600

    Added additional optional inputs and styling.

--- src/signup/Signup.tsx
+++ src/signup/Signup.tsx
@@ -1,6 +1,6 @@
 import './Signup.scss';
 import { useNavigate } from 'react-router-dom';
-import { FaPenFancy, FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
+import { FaPenFancy, FaRegEye, FaRegEyeSlash, FaPlusCircle } from 'react-icons/fa';
 import { useState } from 'react';
 
 function Signup({ setSignedIn }: { setSignedIn: (val: boolean) => void }) {
@@ -36,47 +36,75 @@ function Signup({ setSignedIn }: { setSignedIn: (val: boolean) => void }) {
             <div className='email'>
                 <label htmlFor='email'><span className='asterisk'>*</span>Email:</label>
                 <div>
-                    <input required type="text" id='email' placeholder="Username" />
+                    <input required type="text" id='email' />
                 </div>
                 <label htmlFor='confirm-email'><span className='asterisk'>*</span>Confirm Email:</label>
                 <div>
-                    <input required type="text" id='confirm-email' placeholder="Username" />
+                    <input required type="text" id='confirm-email' />
                 </div>
 
             </div>
             <div className='username'>
                 <label htmlFor='username'><span className='asterisk'>*</span>Username:</label>
                 <div>
-                    <input required type="text" id='username' placeholder="Username" />
+                    <input required type="text" id='username' />
                 </div>
             </div>
             <div className='password'>
                 <label htmlFor='password'><span className='asterisk'>*</span>Password:</label>
                 <div>
-                    <input required type="text" id='password' placeholder="Password" />
+                    <input required type="text" id='password' />
                     <span className='visibility-icon' onClick={toggleVisibility}>
                         {isVisible ? <FaRegEye className='icon'/> : <FaRegEyeSlash className='icon' />}
                     </span>
                 </div>
                 <label htmlFor='confirm-password'><span className='asterisk'>*</span>Confirm Password:</label>
                 <div>
-                    <input required type="text" id='confirm-passowrd' placeholder="Password" />
+                    <input required type="text" id='confirm-passowrd' />
                     <span className='visibility-icon' onClick={toggleVisibility}>
                         {isVisible ? <FaRegEye className='icon'/> : <FaRegEyeSlash className='icon' />}
                     </span>
                 </div>
             </div>
+
             <span className='msg1'>Optional Details</span>
             <span className='goal-msg'>(These details will be displayed on your user profile.)</span>
+            <div className='pronouns'>
+                <label htmlFor='pronouns'>Pronouns:</label>
+                <div>
+                    <input type="text" id='pronouns' />
+                </div>
+            </div>
+            <div className='bio'>
+                <label htmlFor='bio'>Bio:</label>
+                <div className='textarea-container'>
+                    <textarea id='bio' maxLength={250}></textarea>
+                </div>
+            </div>
+            <p>Personal Website</p>
             <div className='website'>
-                <label htmlFor='website-name'>Website</label>
+                <label htmlFor='website-name'>Website name:</label>
                 <div>
-                    <input type="text" id='website-name' placeholder="Site name" />
+                    <input type="text" id='website-name' />
                 </div>
-                <label htmlFor='website-url'>URL</label>
+                <label htmlFor='website-url'>Website URL:</label>
                 <div>
-                    <input type="text" id='website-url' placeholder="URL address" />
+                    <input type="text" id='website-url' />
                 </div>
+            </div>
+            <p>Socials</p>
+            <div className='socials'>
+                <label htmlFor='social-handle'>Handle:</label>
+                <div>
+                    <input type="text" id='social-handle' />
+                </div>
+                <label htmlFor='social-url'>Social URL:</label>
+                <div>
+                    <input type="text" id='social-url' />
+                </div>
+            </div>
+            <div className='add-social'>
+                <FaPlusCircle className='icon' title='Add another social' />
             </div>
             <span className='msg1'>Let's set a few goals...</span>
             <span className='goal-msg'>(Your goals are only visible to you.)</span>
@@ -97,7 +125,6 @@ function Signup({ setSignedIn }: { setSignedIn: (val: boolean) => void }) {
             <span className='goal-msg'>You can update goals at any time from your personal dashboard.</span>
             <button type="submit">Sign up</button>
             <span className='stuck'>
-                <span className='forgot-password'>Forgot password?</span>
                 <span className='no-account' onClick={takeToLoginPage}>Have an account already?</span>
             </span>
         </form>
