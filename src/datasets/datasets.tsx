1c2f8f0da1c07e34f7751ae01eaeb57b0de94186
Jessica Organ <jorgan612@gmail.com>
2026-04-27 15:23:11 -0600

    Added dummy data to begin fleshing out members list on group dashboard.

---
+++ src/datasets/datasets.tsx
@@ -0,0 +1,331 @@
+// removed dummy data. Keeping file for now. Will delete later.
+import { UsersList, User } from '../interfaces/interfaces';
+
+export const user: User = {
+    id: 612,
+    name: 'Jessica',
+    username: 'Jesso',
+    email: 'Jorgan612@gmail.com',
+    password: '123456',
+    pronouns: 'She/Her',
+    bio: 'bookworm | game enthusiast | perpetually curious',
+    joined: 'January 1st, 2026',
+    userIcon: {id: 'coffee', color: '#94a3b8'},
+    website: {
+        name: 'jessoportfolio',
+        url: 'https://jess-o-portfolio.vercel.app/',
+    },
+    socials: [{
+        id: 1,
+        handle: 'jessowrites.bsky.social',
+        url: 'https://bsky.app/profile/jessowrites.bsky.social'
+    },
+    {
+        id: 2,
+        handle: 'jess.o.writes',
+        url: 'https://instagram.com'
+    }],
+    goals: [{
+        name: 'Weekly Word Count',
+        id: '1',
+        total: 3000,
+        current: 0,
+        type: 'word(s)'
+      },
+      {
+        name: 'Weekly Session Frequency ',
+        id: '2',
+        total: 4,
+        current: 0,
+        type: 'day(s)'
+      },
+      {
+        name: 'Overall Word Count',
+        id: '3',
+        total: 90000,
+        current: 0,
+        type: 'word(s)'
+      }],
+    entries: [],
+};
+
+export const users: UsersList = [
+    {
+        id: 612,
+        name: 'Jessica',
+        username: 'Jesso',
+        email: 'Jorgan612@gmail.com',
+        password: '123456',
+        pronouns: 'She/Her',
+        bio: 'bookworm | game enthusiast | perpetually curious',
+        joined: 'January 1st, 2026',
+        userIcon: {id: 'coffee', color: '#94a3b8'},
+        website: {
+            name: 'jessoportfolio',
+            url: 'https://jess-o-portfolio.vercel.app/',
+        },
+        socials: [{
+            id: 1,
+            handle: 'jessowrites.bsky.social',
+            url: 'https://bsky.app/profile/jessowrites.bsky.social'
+        },
+        {
+            id: 2,
+            handle: 'jess.o.writes',
+            url: 'https://instagram.com'
+        }],
+        goals: [{
+            name: 'Weekly Word Count',
+            id: '1',
+            total: 3000,
+            current: 0,
+            type: 'word(s)'
+        },
+        {
+            name: 'Weekly Session Frequency ',
+            id: '2',
+            total: 4,
+            current: 0,
+            type: 'day(s)'
+        },
+        {
+            name: 'Overall Word Count',
+            id: '3',
+            total: 90000,
+            current: 0,
+            type: 'word(s)'
+        }],
+        entries: [],
+    },
+    {
+        id: 311,
+        name: 'Max',
+        username: 'MaxExplores',
+        email: 'mhenrey091@gmail.com',
+        password: '654321',
+        pronouns: 'He/Him',
+        bio: 'I LIKE SPAAAAAAAACE!',
+        joined: 'April 27th, 2026',
+        userIcon: {id: 'moon', color: '#74c3b0'},
+        website: {
+            name: 'MaxExploresLife',
+            url: 'https://jess-o-portfolio.vercel.app/',
+        },
+        socials: [{
+            id: 1,
+            handle: 'maxexplores',
+            url: 'https://instagram.com'
+        },
+        {
+            id: 2,
+            handle: 'maxexplorestactical',
+            url: 'https://instagram.com'
+        }],
+        goals: [{
+            name: 'Weekly Word Count',
+            id: '1',
+            total: 500,
+            current: 0,
+            type: 'word(s)'
+        },
+        {
+            name: 'Weekly Session Frequency ',
+            id: '2',
+            total: 1,
+            current: 0,
+            type: 'day(s)'
+        },
+        {
+            name: 'Overall Word Count',
+            id: '3',
+            total: 50000,
+            current: 0,
+            type: 'word(s)'
+        }],
+        entries: [],
+    },
+    {
+        id: 123,
+        name: 'Lizzy',
+        username: 'Ms. Bennet',
+        email: 'test@gmail.com',
+        password: '001589',
+        pronouns: 'She/Her',
+        bio: 'What are men to rocks and mountains',
+        joined: 'March  5th, 2026',
+        userIcon: {id: 'penFancy', color: '#f5f5f5'},
+        website: {
+            name: 'AustenNet',
+            url: 'https://jess-o-portfolio.vercel.app/',
+        },
+        socials: [{
+            id: 1,
+            handle: 'jessowrites.bsky.social',
+            url: 'https://bsky.app/profile/jessowrites.bsky.social'
+        },
+        {
+            id: 2,
+            handle: 'jess.o.writes',
+            url: 'https://instagram.com'
+        }],
+        goals: [{
+            name: 'Weekly Word Count',
+            id: '1',
+            total: 3000,
+            current: 0,
+            type: 'word(s)'
+        },
+        {
+            name: 'Weekly Session Frequency ',
+            id: '2',
+            total: 4,
+            current: 0,
+            type: 'day(s)'
+        },
+        {
+            name: 'Overall Word Count',
+            id: '3',
+            total: 90000,
+            current: 0,
+            type: 'word(s)'
+        }],
+        entries: [],
+    },
+    {
+        id: 1774,
+        name: 'Jamie',
+        username: 'JamieFrayfray',
+        email: 'Jorgan612@gmail.com',
+        password: '123456',
+        pronouns: 'He/Him',
+        bio: 'Oh aye, Sassenach.',
+        joined: 'January 1st, 2026',
+        userIcon: {id: 'horseHead', color: '#f18581'},
+        website: {
+            name: 'HighlanderMan',
+            url: 'https://jess-o-portfolio.vercel.app/',
+        },
+        socials: [{
+            id: 1,
+            handle: 'jessowrites.bsky.social',
+            url: 'https://bsky.app/profile/jessowrites.bsky.social'
+        },
+        {
+            id: 2,
+            handle: 'jess.o.writes',
+            url: 'https://instagram.com'
+        }],
+        goals: [{
+            name: 'Weekly Word Count',
+            id: '1',
+            total: 0,
+            current: 0,
+            type: 'word(s)'
+        },
+        {
+            name: 'Weekly Session Frequency ',
+            id: '2',
+            total: 0,
+            current: 0,
+            type: 'day(s)'
+        },
+        {
+            name: 'Overall Word Count',
+            id: '3',
+            total: 0,
+            current: 0,
+            type: 'word(s)'
+        }],
+        entries: [],
+    },
+    {
+        id: 1818,
+        name: 'Frankenstein',
+        username: 'Frankie',
+        email: 'Jorgan612@gmail.com',
+        password: '123456',
+        pronouns: 'He/Him',
+        bio: 'I made a mistake',
+        joined: 'January 1st, 2026',
+        userIcon: {id: 'skull', color: '#94a3b8'},
+        website: {
+            name: 'frankensteinsCreation',
+            url: 'https://jess-o-portfolio.vercel.app/',
+        },
+        socials: [{
+            id: 1,
+            handle: 'frankenstein.bsky.social',
+            url: 'https://bsky.app/profile/jessowrites.bsky.social'
+            },
+        ],
+        goals: [{
+            name: 'Weekly Word Count',
+            id: '1',
+            total: 3000,
+            current: 0,
+            type: 'word(s)'
+        },
+        {
+            name: 'Weekly Session Frequency ',
+            id: '2',
+            total: 4,
+            current: 0,
+            type: 'day(s)'
+        },
+        {
+            name: 'Overall Word Count',
+            id: '3',
+            total: 90000,
+            current: 0,
+            type: 'word(s)'
+        }],
+        entries: [],
+    },
+    {
+        id: 1993,
+        name: 'Dr. Grant',
+        username: 'Bone Guy',
+        email: 'Jorgan612@gmail.com',
+        password: '123456',
+        pronouns: 'He/Him',
+        bio: 'They do move in herds',
+        joined: 'January 1st, 2026',
+        userIcon: {id: 'bone', color: '#94a3b8'},
+        website: {
+            name: 'I dig it',
+            url: 'https://jess-o-portfolio.vercel.app/',
+        },
+        socials: [{
+            id: 1,
+            handle: 'DrG.bsky.social',
+            url: 'https://bsky.app/profile/jessowrites.bsky.social'
+        },
+        {
+            id: 2,
+            handle: 'bone.guy1993',
+            url: 'https://instagram.com'
+        }],
+        goals: [{
+            name: 'Weekly Word Count',
+            id: '1',
+            total: 3000,
+            current: 0,
+            type: 'word(s)'
+        },
+        {
+            name: 'Weekly Session Frequency ',
+            id: '2',
+            total: 4,
+            current: 0,
+            type: 'day(s)'
+        },
+        {
+            name: 'Overall Word Count',
+            id: '3',
+            total: 90000,
+            current: 0,
+            type: 'word(s)'
+        }],
+        entries: [],
+    },
+];
