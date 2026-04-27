1397ebd2170b4ed95fa4bf072de2ee4876cdbb2d
Jessica Organ <jorgan612@gmail.com>
2026-04-27 15:24:09 -0600

    Added new files for members list on group dashboard. Began fleshing out structure of card and styling.

---
+++ src/members/Members.tsx
@@ -0,0 +1,29 @@
+import { MembersProps } from '../interfaces/interfaces';
+import { userIcons, userIconColor } from '../assets/icons/userIcons/userIcons';
+import { FaRegUserCircle } from 'react-icons/fa';
+import './Members.scss';
+
+function Members({users}: MembersProps) {
+
+    return (
+        <div className="members-list">
+            <h3>Members</h3>
+            {users.map((user) => {
+                const iconData = userIcons.find(icon => icon.id === user.userIcon.id);
+                const PreviewIcon = iconData?.icon || FaRegUserCircle;
+                const previewColor = user.userIcon?.color || '#94a3b8';
+                return (
+                    <div className='member-card'>
+                        <div className='user-icon-name'>
+                            <PreviewIcon className='icon' style={{color: previewColor}} />
+                            <label>{user.name}</label>
+                        </div>
+
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Members;
