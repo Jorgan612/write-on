7a8fef302228febab45e6bd636d4cdc505674a1c
Jessica Organ <jorgan612@gmail.com>
2026-05-07 13:11:17 -0600

    Added temp dummydate to flesh out sign up columns styling. Beginning styling of sign up columns.

--- src/groupSignUp/GroupSignUp.tsx
+++ src/groupSignUp/GroupSignUp.tsx
@@ -1,9 +1,47 @@
 import './GroupSignUp.scss';
+import { user1, user2 } from '../datasets/datasets';
 
 function GroupSignUp() {
+    const dates = [
+        {
+            id: '1',
+            date: '2026-05-07',
+            signups: [user1, user2]
+        },
+        {
+            id: '2',
+            date: '2026-05-08',
+            signups: [user1, user2]
+        },
+        {
+            id: '3',
+            date: '2026-05-09',
+            signups: [user1, user2]
+        },
+        {
+            id: '4',
+            date: '2026-05-010',
+            signups: [user1, user2]
+        }
+    ];
+
+
     return (
         <div className='sign-up'>
-            <p>SIGN UP</p>
+            {dates.map((date) => {
+                return (
+                    <div className='column'>
+                        <h3>{date.date}</h3>
+                        <div className='options-header'>
+
+                        </div>
+                        <div className='sign-up-list'>
+
+                        </div>
+
+                    </div>
+                )
+            })}
         </div>
     )
 }
