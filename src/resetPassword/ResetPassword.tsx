a44247fcd8089055d58154b9436b29b9390e85e4
Jessica Organ <jorgan612@gmail.com>
2026-05-26 17:58:32 -0600

    Added new ResetPassword component and scss file. imported into App to hook up new route.

---
+++ src/resetPassword/ResetPassword.tsx
@@ -0,0 +1,11 @@
+import './ResetPassword.scss';
+
+function ResetPassword() {
+    return(
+        <div className='reset-password-page'>
+            TEST RESET PASSWORD PAGE
+        </div>
+    )
+}
+
+export default ResetPassword;
