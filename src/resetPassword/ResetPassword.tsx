df9c59c8b88c1e5e892842712ab13e9ab9ad789a
Jessica Organ <jorgan612@gmail.com>
2026-05-26 19:16:58 -0600

    Added styling for page and functionality to reset password. Need to continue hooking up to backend.

--- src/resetPassword/ResetPassword.tsx
+++ src/resetPassword/ResetPassword.tsx
@@ -1,10 +1,50 @@
+import { FormEvent, useState } from 'react';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import './ResetPassword.scss';
 
 function ResetPassword() {
+    const [inputPassword, setInputPassword] = useState<string>('');
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    const submitNewPassword = async (e: FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:5000/reset-password', {
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json',
+                },
+                body: JSON.stringify({ password: inputPassword }),
+            });
+
+            const data = await response.json();
+
+            if  (response.ok) {
+                alert(data.message || 'Password reset successfully! You may now log into your account using the new password.');
+            }
+
+        } catch (err) {
+            console.error('Error resetting password:', err);
+            console.error('Something went wrong while resetting your password. Please try again.');
+        }
+    };
+
+
     return(
-        <div className='reset-password-page'>
-            TEST RESET PASSWORD PAGE
-        </div>
+        <form className="reset-password-page" onSubmit={submitNewPassword}>
+            <h1>Write On</h1>
+            <h2>Password Reset</h2>
+            <div>
+                <label htmlFor="password">Please enter a new password for your account.<span>*</span></label>
+                <input required type={isVisible ? 'text' : 'password'} id="password" placeholder="password" autoComplete="password" value={inputPassword} onChange={(e) => setInputPassword(e.target.value)}/>
+                <span className='visibility-icon' 
+                    onClick={() => setIsVisible(prev => !prev)}>
+                    {isVisible ? <FaRegEye className='icon'/> : <FaRegEyeSlash className='icon' />}
+                </span>
+            </div>
+            <button type="submit" disabled={!inputPassword} title={inputPassword ? '' : 'Enter a new password'}>Reset</button>
+        </form>
     )
 }
 
